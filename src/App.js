12bb686f4c0a592ad52da46c9813e064fd4f547f
Ricardo Salazar <salazar.r31@outlook.es>
2022-12-27 16:59:33 -0600

    Updated AccountSetUp module

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import * as React from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
-import Header from './views/softLaunch/components/Header';
-import Main from "./views/softLaunch/Main";
 import LogIn from "./views/logIn/LogIn";
 import ForgotPassword from "./views/password/ForgotPassword";
 import ChangePassword from "./views/password/ChangePassword";
-import About from "./views/softLaunch/About";
 import AccountSetUp from "./views/accountSetUp/AccountSetUp";
-/*
-<LogIn/>
-*/
 export default function App() {
     const [subscription, setSubscription] = React.useState(false);
     console.log(subscription);
     return (
         <div className="bg-green-2 h-full md:screen md:h-screen font-nunito">
             <Routes>
-                <Route path="/ourstory" element = {<About/>}/>
                 <Route path="/signup" element = {<AccountSetUp/>}/>
                 <Route path="/" element = {<LogIn/>}/>
                 <Route path="/forgotpassword" element={<ForgotPassword/>}/>
                 <Route path="/changepassword" element = {<ChangePassword/>}/>
-                <Route path="/about" element = {<About/>}/>
+                <Route path="/accountsetup" element = {<AccountSetUp/>}/>
             </Routes>
             
         </div>
