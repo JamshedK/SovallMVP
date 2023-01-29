5813680202a815193c75fafe51ec0d63a7d191c1
JamshedK <karimnazarovj@gmail.com>
2023-01-29 16:34:30 -0600

    added .env file for firebase config keys

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,17 +4,18 @@ import {getFirestore} from '@firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCanACeDK7fsTwEPlfJDgehm9M2RFck9FA",
-  authDomain: "sovall-a20af.firebaseapp.com",
-  projectId: "sovall-a20af",
-  storageBucket: "sovall-a20af.appspot.com",
-  messagingSenderId: "207158666780",
-  appId: "1:207158666780:web:50fa5ece838e256b8461d9",
-  measurementId: "G-1EBYHY6YLY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
 
 // Initialize db
 export const db = getFirestore();
--- src/views/loader/Loaders.js
+++ src/views/loader/Loaders.js
@@ -4,7 +4,7 @@ const Loader = () =>{
     return(
         <div style={{backgroundColor: "#3C9A9A"}} className='h-full w-full flex grow'>
             <div className='border-2 mx-auto my-auto align-bottom gap-2 pb-10 flex flex-row text-white font-bold text-lg'>
-                <p className='box-border border-2'>Loading your</p>
+                <p className='box-border border-2 pb-0'>Loading your</p>
                 <img src = {sovall} className="w-30% h-30% border-2" />
                 <p className=' box-border border-2'>adventure</p>
             </div>
