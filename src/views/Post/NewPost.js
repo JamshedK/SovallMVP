b4f9f1de632e7d232d08847cc80328f544cfb383
JamshedK <karimnazarovj@gmail.com>
2023-02-12 18:00:31 -0600

    Added categories

--- src/views/Post/NewPost.js
+++ src/views/Post/NewPost.js
@@ -7,11 +7,13 @@ import { useContext, useState, useRef } from 'react';
 import { db, storage } from '../../firebase-config';
 import { collection, addDoc} from '@firebase/firestore';
 import {ref, uploadBytes} from 'firebase/storage'
+
 // The page for creating a new post
 const NewPost = () => {
     const authCtx = useContext(AuthContext);
     const postTextRef = useRef();       // useRef hook to get reference for the textArea and get it's content later on
     const imageRef = useRef();
+    const selectCategoryRef = useRef();
     const postCollectionRef = collection(db, 'posts')
     
     // Save the user post post to Firebase
@@ -22,10 +24,11 @@ const NewPost = () => {
         if(imageRef.current.files[0]){
             containsImage = true;
         }
-        // generate a random number to be added to the name of the image
-        const randomNum = Math.round(Math.random()*1000)
+
         var imagePath = '';
         if(containsImage){
+            // generate a random number to be added to the name of the image
+            const randomNum = Math.round(Math.random()*1000)
             // path for the image to be saved
             imagePath = `posts/${imageRef.current.files[0].name + randomNum}`
             // store the image in firebase
@@ -36,7 +39,8 @@ const NewPost = () => {
             userID: authCtx.userID,
             text: postTextRef.current.value,
 			published_date: published_date,
-            imagePath:imagePath
+            imagePath:imagePath,
+            category:selectCategoryRef.current.value
 		})
 
     }
@@ -63,7 +67,15 @@ const NewPost = () => {
                         <p>Name</p>
                         <p>Date</p>
                     </div>
-                    <p>No tab selected</p>
+                    <select ref = {selectCategoryRef} defaultValue={'DEFAULT'}>
+                        <option value="DEFAULT" disabled>No category selected</option>
+                        <option value='Problems'>Problems</option>
+                        <option value='Solutions'>Solutions</option>
+                        <option value='Resources'>Resources</option>
+                        <option value='Opportunities'>Opportunities</option>
+                        <option value='Polls'>Polls</option>
+                        <option value='Other'>Other</option>
+                    </select>
                 </div>
                 {/* text area */}
                 <div className='flex flex-col gap-5'>
@@ -78,7 +90,7 @@ const NewPost = () => {
                     <input type="file" accept="image/png, image/jpeg" ref={imageRef}/>
                     {/* TODO: Can add the poll here later */}
                 </div>
-                {/* post */}
+                {/* post button */}
                 <button onClick={handlePost} className='bg-[#025B5B] text-white'>Post</button>
             </div>
         </div>
