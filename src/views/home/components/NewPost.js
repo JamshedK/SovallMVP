3b52d5029c723d852afaf90d9d1ac7e5f256ee94
obedewesley <obedenatana@gmail.com>
2023-03-17 19:08:21 +0300

    NewPost Update as well

--- src/views/home/components/NewPost.js
+++ src/views/home/components/NewPost.js
@@ -86,7 +86,7 @@ const NewPost = (props) => {
    ● Find a team member\n   ● Launch a poll and collect data'
 
     return (
-        <div className={"w-full h-fit flex flex-col border border-dashed border-gray-300 rounded-xl bg-white gap-6 p-10 " + props.width }>
+        <div className={"relative w-full h-fit flex flex-col border border-dashed border-gray-300 rounded-xl bg-white gap-6 p-8 px-8 " + props.width }>
             <div className="text-green-1">
                 {/* <label className="font-semibold" >We are here to help you grow. Venture towards excellence</label>
                 <ul className="list-disc pl-7 w-full cursor-text" onClick={handleClick}>
@@ -95,17 +95,17 @@ const NewPost = (props) => {
                     <li>Find a team...</li>
                 </ul> */}
                 {/*profile and selecting category for the post*/ }
-                <div className='flex flex-row items-start '>
-                    <img className="w-9 h-9 rounded-full" src={profile} />
-                    <div className='text-[13px] px-1'>
-                        <p className='font-bold'> Wesley</p>
-                        <p>Sep 29, 2022</p>
+                <div className='relative flex flex-row items-start'>
+                    <img className="w-10 h-10 rounded-full" src={profile} />
+                    <div className='px-1'>
+                        <p className='text-[14px] font-bold'> Wesley</p>
+                        <p className='text-[12px]'>Sep 29, 2022</p>
                     </div>
 
                 
 
-                    <select className="relative w-38 h-8 align-text-top text-left text-sm top-6 right-2 rounded-[20px]" ref = {selectCategoryRef}defaultValue={'DEFAULT'}>
-                        <option value="DEFAULT" disabled>No tab selected</option>
+                    <select className="absolute top-[0px] w-35 h-9 align-text-top text-top text-[12px] right-0 rounded-[20px]" ref = {selectCategoryRef}defaultValue={'DEFAULT'}>
+                        <option className='' value="DEFAULT" disabled>No tab selected</option>
                         <option value='Problems'>Problems</option>
                         <option value='Solutions'>Solutions</option>
                         <option value='Resources'>Resources</option>
@@ -119,30 +119,37 @@ const NewPost = (props) => {
                 {/* text area 
                 Text box automatic resizing with minimum of two lines.*/}
                 <div className='flex flex-col gap-5'>
-                    <textarea placeholder={placeholder} className='relative border-hidden rounded-[14.5px] w-[490px] h-[200px] top-8 placeholder:p-4 placeholder:px-[111px]'
+                    <textarea placeholder={placeholder} className='relative border-hidden rounded-[14.5px] w-[430px] h-[160px] top-2 placeholder:text-[15px] placeholder:px-[11px]'
                      ref={postTextRef}>
                     </textarea>
                     {containsImage && <img src = {imagePath}></img>}
                 </div>
 
                 {/* post button */}
-                <button onClick={handlePost} className='relative left-[200px] top-10 rounded-[14.5px] w-20 h-7 text-[14px] place-items-center bg-[#025B5B] text-white'>Post</button>
-                
-            </div>
-            
-            {/* upload options */}
-            <div className="gap-6 w-fit h-full">
+
+                <div className='relative flex flex-row left-[160px] top-9 mb-4'>
+                    <button onClick={handlePost} className=' rounded-[14.5px] w-20 h-7 text-[14px] bg-[#025B5B] text-white'>Post</button>
+
+                    <div className="relative left-[170px] top-1 w-fit h-full">
                     {/* TODO: Correct the styles */}
                     
-                    <label for='file-input' className='relative top-10 flex flex-row gap-6 pointer-events-auto left-[450px]'>
-                        <img src={doc}></img>
-                        <input id='file-input' className='invisible' type="file" accept="image/png, image/jpeg" ref={imageRef} onChange = {handleImageSelected}/>
+                        <label for='file-input' className='w-fit h-fit pointer-events-auto'>
+                            <img src={doc}></img>
+                            <input id='file-input' className='invisible w-4 h-4' type="file" accept="image/png, image/jpeg" ref={imageRef} onChange = {handleImageSelected}/>
 
-                    </label>
+                        </label>
 
 
                     {/* TODO: Can add the poll here later */}
+                    </div>
+
+                </div>
+
+                
+                
+
             </div>
+            
         </div>
     );
 }
